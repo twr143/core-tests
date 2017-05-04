a5bab98ce2250b1ff2f42839c173b99a08f2f11a
AlexSmith1988 <alexey.ig.kuznetsov@gmail.com>
2017-05-04 14:05:56 +0300

    Test

--- src/cas/FibEntry.java
+++ src/cas/FibEntry.java
@@ -7,98 +7,113 @@ import java.util.concurrent.atomic.AtomicLong;
 /**
  * Created by ilya on 04.05.2017.
  */
-public class FibEntry
-{
-  public static void main(String[] args) throws InterruptedException
-  {
-    ExecutorService es = Executors.newFixedThreadPool(3);
-    FibLongCAS fb = new FibLongCAS();
-    int iterationNumber=17;
-    boolean bPrintIntermediate=true;
-    Runnable r1 = new FibLongCASTask(iterationNumber,fb,bPrintIntermediate);
-    Runnable r2 = new FibLongCASTask(iterationNumber,fb,bPrintIntermediate);
-    Runnable r3 = new FibLongCASTask(iterationNumber,fb,bPrintIntermediate);
-    es.submit(r1);
-    es.submit(r2);
-    es.submit(r3);
-    es.shutdown();
-    Thread.sleep(300);
+public class FibEntry {
 
-    FibLongSingleThreaded fbST = new FibLongSingleThreaded();
-    long fbSTResult=0;
-    for (int i=0;i<iterationNumber*3;i++)
-      fbSTResult=fbST.nextVal();
-    System.out.println("singlethreaded:\t" +fbSTResult);
-    System.out.println("multithreaded:\t" +fb.get());
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService es = Executors.newFixedThreadPool(3);
+        FibLongCAS fb = new FibLongCAS();
+        int iterationNumber = 17;
 
-    assert fb.get()==fbST.get();
-  }
-  static class FibLongCAS
-  {
-    private AtomicLong val1=new AtomicLong(1);
-    private AtomicLong val2=new AtomicLong(1);
+        es.submit(new FibLongCASTask(iterationNumber, fb));
+        es.submit(new FibLongCASTask(iterationNumber, fb));
+        es.submit(new FibLongCASTask(iterationNumber, fb));
+        es.shutdown();
 
-    public long nextVal(){
+        Thread.sleep(300);
 
-      AtomicLong prevToUpdate;
-      long next,locval1,locval2,locprev;
-      do {
-        locval1=val1.get();locval2=val2.get();
-        if (locval2>locval1){
-          prevToUpdate = val1;
-          locprev=locval1;
+        FibLongSingleThreaded fbST = new FibLongSingleThreaded();
+        long fbSTResult = 0;
+        for (int i = 0; i < iterationNumber * 3; i++) {
+            fbSTResult = fbST.nextVal();
         }
-        else{
-          prevToUpdate=val2;
-          locprev=locval2;
+
+        System.out.println("singlethreaded:\t" + fbSTResult);
+        System.out.println("multithreaded:\t" + fb.get());
+
+        assert fb.get() == fbST.get();
+    }
+
+    static class FibLongCAS2 {
+        private AtomicLong oneAtomic = new AtomicLong(0);
+        private AtomicLong anotherAtomic = new AtomicLong(1);
+
+        public long nextVal() {
+            boolean anotherIsBigger;
+            long one, another, next;
+            do {
+                one = oneAtomic.get();
+                another = anotherAtomic.get();
+
+                next = one + another;
+
+                anotherIsBigger = one < another;
+            } while (!(anotherIsBigger ? oneAtomic.compareAndSet(one, next) : anotherAtomic.compareAndSet(another, next)));
+            return next;
         }
-        next=locval1+locval2;
-      } while (!prevToUpdate.compareAndSet(locprev,next ));
-      return next;
-    }
-    public long get(){
-      return (val2.get()>val1.get()?val2.get():val1.get());
-    }
-  }
-  static class FibLongCASTask implements Runnable{
 
-    int iters;
-    FibLongCAS fb;
-    boolean bPrintIntermediate;
-    public FibLongCASTask(int iters, FibLongCAS fb, boolean bPrintIntermediate)
-    {
-      this.iters = iters;
-      this.fb = fb;
-      this.bPrintIntermediate = bPrintIntermediate;
     }
-    @Override
-    public void run()
-    {
-        int i=0;
-      while (i++ < iters)
-        if (bPrintIntermediate)
-          System.out.println("thread: "+Thread.currentThread().getName()+" next:\t" + fb.nextVal());
-        else
-          fb.nextVal();
-    }
-  }
-  static class FibLongSingleThreaded
-  {
-    private long val1=1L;
-    private long val2=1L;
 
-    public long nextVal(){
+    static class FibLongCAS {
+        private AtomicLong val1 = new AtomicLong(1);
+        private AtomicLong val2 = new AtomicLong(1);
 
-      long next=val1+val2;
-      if (val2>val1)
-        val1=next;
-      else
-        val2=next;
-      return next;
+        public long nextVal() {
+            AtomicLong prevToUpdate;
+            long next, locval1, locval2, locprev;
+            do {
+                locval1 = val1.get();
+                locval2 = val2.get();
+                if (locval2 > locval1) {
+                    prevToUpdate = val1;
+                    locprev = locval1;
+                } else {
+                    prevToUpdate = val2;
+                    locprev = locval2;
+                }
+                next = locval1 + locval2;
+            } while (!prevToUpdate.compareAndSet(locprev, next));
+            return next;
+        }
+
+        public long get() {
+            return (val2.get() > val1.get() ? val2.get() : val1.get());
+        }
     }
-    public long get(){
-      return (val2>val1?val2:val1);
+
+    static class FibLongCASTask implements Runnable {
+        int iters;
+        FibLongCAS fb;
+
+        public FibLongCASTask(int iters, FibLongCAS fb) {
+            this.iters = iters;
+            this.fb = fb;
+        }
+
+        @Override
+        public void run() {
+            int i = 0;
+            while (i++ < iters)
+                System.out.println("thread: " + Thread.currentThread().getName() + " next:\t" + fb.nextVal());
+        }
     }
-  }
+
+    static class FibLongSingleThreaded {
+        private long val1 = 1L;
+        private long val2 = 1L;
+
+        public long nextVal() {
+
+            long next = val1 + val2;
+            if (val2 > val1)
+                val1 = next;
+            else
+                val2 = next;
+            return next;
+        }
+
+        public long get() {
+            return (val2 > val1 ? val2 : val1);
+        }
+    }
 
 }
