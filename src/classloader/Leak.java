42fbcf226334da3913a706cce6ce7f2a8dc7481c
AlexSmith1988 <alexey.ig.kuznetsov@gmail.com>
2017-05-26 15:51:25 +0300

    Leak

--- src/classloader/Leak.java
+++ src/classloader/Leak.java
@@ -11,17 +11,12 @@ public class Leak {
             @Override
             public void run() {
                 while (true) {
-                    try {
-                        Thread.sleep(10);
                         new ThreadLocal<Object>() {
                             int[] moreBytesToLeak = new int[1024 * 1024 * 25];
                             {
                                 set(this);
                             }
                         };
-                    } catch (InterruptedException e) {
-                        System.out.println("Interrupted");
-                    }
                 }
             }
         };
