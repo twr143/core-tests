6a481e0584c96a97465bf9931802d179d9fc8310
AlexSmith1988 <alexey.ig.kuznetsov@gmail.com>
2017-05-26 17:16:34 +0300

    Leak

--- src/leaks/LeakThreadLocal.java
+++ src/leaks/LeakThreadLocal.java
@@ -1,35 +1,52 @@
 package leaks;
 
+import java.io.IOException;
+
 /**
  * Created by AIKuznetsov on 26.05.2017.
  */
-public class LeakThreadLocal
-{
+public class LeakThreadLocal {
 
-    static int count;
-    public static void main(String[] args) throws InterruptedException {
+    // True for causing OOM, false to leaking 1 GB of HEAP without GC possibility
+    static final boolean causeOOM = false;
+
+    public static void main(String[] args) throws InterruptedException, IOException {
+        // Here is the time to go to your profiler and hook up to the app to monitor heap / call GC manually
+        System.out.println("Please hook up a profiler to this App JVM, then press any key...");
+        System.in.read();
 
         final Thread thread = new Thread(() -> {
-            while (true) {
-                new ThreadLocal<Object>() {
-                    int[] moreBytesToLeak = new int[1024 * 1024 * 25];
+            long count = 0;
 
-                    {
-                        set(this);
-                      System.out.print((count++)+" ");
+            while (true) {
+                // If we want to cause OOM we just create relentlessly, otheerwise we eat up about 1 GB of HEAP to see in profiler and manually call GC
+                if (causeOOM || count < 1000000) {
+                    new ThreadLocal<Object>() {
+                        int[] moreBytesToLeak = new int[256];
+
+                        {
+                            set(this);
+                        }
+                    };
+                }
+
+                ++count;
+
+                // Print out current count
+                if (count % 10000 == 0) {
+                    System.out.println(causeOOM || count < 1000000 ? count : "No More");
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        System.out.println("How did we get it?");
                     }
-                };
-                try
-                {
-                    Thread.sleep(20);
                 }
-                catch (InterruptedException e)
-                {
-                    e.printStackTrace();
-                }
+
+
             }
         });
         thread.start();
         thread.join();
+
     }
 }
